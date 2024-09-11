fccb9d8a71352c50c64feb46293a4a66aaec545d
Sophie <wolvesgo@gmail.com>
2024-09-12 00:29:24 +0200

    initial commit

---
+++ carcassonne.py
@@ -0,0 +1,31 @@
+# carcassonne
+
+# DetaIls: 
+#     Romanesque buildings
+#     Gardens
+#     Lady carcas, wife of ballac, a muslim prince
+#     ballak was killed in a war against charlemagne
+#     LC took charge of carcassonnes defence systems
+#     repelled the army by putting scarecrows in the watchtowers and throwing a pig stuffed with wheat out a window
+#     siege lasted 5 years
+#     charlemagne thought city was overflowing with soldiers and food, so he ran away
+#     carcassonne translate to carcas is ringing which is what they did when they won
+#     year    8th century
+#     population 10000 ish
+#     food reserves lasted   :But by the beginning of the sixth year, food and water were becoming increasingly scarce.
+#      3 kilometres (1.9 mi) long double surrounding walls interspersed by 52 towers
+#      Lady Carcas wanted to make an inventory of all the reserves left. The city was Saracen, so a part of the population, being Muslim, did not consume pork. The villagers brought her a pig and a bag of wheat. She then had the idea of feeding the pig with the sack of wheat and then pushing it from the highest tower of the city at the foot of the outer ramparts.
+
+
+
+# dress style:    look it up
+
+# Game plot:
+#     Lady Carcas or Lord Ballak has to defend city (Carcassonne = Carcas is ringing) from Charlemagne's army. 
+        # Has the option of solving this problem multiple ways. 
+        #     >Scare them off ---> can either use the cow, the soldiers, or a roaring noise and explosions to make them think that there is a fire inside
+        #     >Sneak out of the city ---> dig a tunnel, dress up as warriors, cows, grass
+        #     >die ---> starve, give in.
+        #     >fight them off in battle ----> needs no explanation
+
+# game stars in carcassonne before the invasion. Lord/Lady has to go to a meeting, and during the meeting a messenger announces that they are under siege
